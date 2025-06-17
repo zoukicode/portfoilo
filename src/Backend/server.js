b7b062ed5836e04216ab100f9a055c3c62a15d8b
Zoukicode <marzoukagrignan@gmail.com>
2025-06-17 16:55:48 +0200

    'last'

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -1,31 +1,22 @@
-import mysql from 'mysql2'
-import dotenv from 'dotenv'
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
+
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-})
+});
 
-// connexion a la base de donnée
+db.connect((err) => {
+  if (err) {
+    console.error('Échec de connexion à la base de donnée', err);
+    process.exit(1); // ou: throw new Error('...')
+  }
+  console.log('Connexion réussie');
+});
 
-// const db = mysql.createConnection(
-//     {
-//         user:'root',
-//         password:'',
-//         database:'portfoillo',
-//         host:'localhost'
-//     }
-// )
-db.connect((err)=>{
-    if(err){
-         throw new console.error('echec de connexion a la base de donnée')
-    }
-    console.log('connexion reussi')
-    
-})
-
-export default db
+export default db;
