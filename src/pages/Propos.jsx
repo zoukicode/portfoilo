a0e65b765a18548b30357dbea33b0d503bce5bad
Zoukicode <marzoukagrignan@gmail.com>
2025-06-18 14:45:49 +0200

    'apdate'

--- src/pages/Propos.jsx
+++ src/pages/Propos.jsx
@@ -11,14 +11,14 @@ export function Propos({text,langue,setLangue}){
                 {text.propos.titre} {/*titre de la section*/}
                 </motion.h1>
 <div class=" border-l-3 border-[#263b7a] pl-4 flex flex-col gap-2">
-            <p class="before:rounded-[50%] relative before:w-5 before:h-5 before:absolute before:bg-[#0EA5E9] before:content-[''] before:left-[-26px]">
+            <p class="before:rounded-[50%] relative before:w-5 before:h-5 before:absolute before:bg-[#0EA5E9] before:content-[''] before:left-[-27px]">
 {text.propos.introduction}
  </p>
-<p class="before:rounded-[50%] relative before:w-5 before:h-5 before:absolute before:bg-[#0EA5E9] before:content-[''] before:left-[-26px]">
+<p class="before:rounded-[50%] relative before:w-5 before:h-5 before:absolute before:bg-[#0EA5E9] before:content-[''] before:left-[-27px]">
     {text.propos.objectif}
     </p>
 
-<div className="before:rounded-[50%] relative before:w-5 before:h-5 before:absolute before:bg-[#0EA5E9] before:content-[''] before:left-[-26px]">
+<div className="before:rounded-[50%] relative before:w-5 before:h-5 before:absolute before:bg-[#0EA5E9] before:content-[''] before:left-[-27px]">
   <p>{text.propos.service}</p>
   {text.propos.type.map((v, i) => (
     <p class="pl-5" key={i}>{v}</p>
